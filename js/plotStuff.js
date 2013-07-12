20d7896cb0ffe4e5303e1703b9c4d076712c088a
mtoth <mtoth@yahoo-inc.com>
2013-07-12 09:14:56 -0700

    Added d3 so a black circle is printed for each element in returned JSON list

--- js/plotStuff.js
+++ js/plotStuff.js
@@ -3,40 +3,21 @@ $(document).ready(function(){
 
         $.getJSON('../BandSearch/parse.php', {'artist': $("#artist").val()}, function(response){
 
-        console.log(response);
-
         var width = 960;
         var height = 500;
 
         var svg = d3.select("body").append("svg")
         			.attr("width", width)
-        			.attr("height", height)
+        			.attr("height", height);
 
         	// Put D3 stuff here!
-		d3.select("body").selectAll("p")
+		var circles = svg.selectAll("circle")
 		    .data(response)
-		  	.enter()
-		  	.append("p")
-		    .text(function(response) { return response['name']; });
+		  	.enter().append("circle");
 
-		var node = svg.selectAll(".node")
-					.data(response)
-				.enter().append("circle")
-					.attr("class", "node")
-					.attr("r", 30)
-					.attr("cx", 100)
-					.attr("cy", 50)
-					.attr("stroke","black")
-					.attr("fill", "white");
-
-		/*		
-		node.append("text")
-			.data(response)
-			.attr("dx", 100)
-			.attr("dy", 100)
-			.text(function(response) {return response['name']; });
-			*/
-
+		circles.attr("cx", function(d, i) {return (i * 50) + 25;})
+			.attr("cy", function() {return 25;})
+			.attr("r", function() {return 10;});
 
         });
     });
